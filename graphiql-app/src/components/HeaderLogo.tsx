dc7ae5cd7609d072a81a0edc6f219fc4e59a8498
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-05-06 13:36:17 +0300

    feat: changes animation based on #12

--- graphiql-app/src/components/HeaderLogo.tsx
+++ graphiql-app/src/components/HeaderLogo.tsx
@@ -5,23 +5,23 @@ export const HeaderLogo = () => {
   return (
     <Link to="/" className="headerLogo__wrapper">
       <div className="headerLogo__imgssss">
-        <div className="circle-big circle-top"></div>
-        <div className="circle-small circle-left-top"></div>
-        <div className="circle-small circle-right-top"></div>
-        <div className="circle-small circle-center"></div>
-        <div className="circle-big circle-left-bottom"></div>
-        <div className="circle-big circle-right-bottom"></div>
-        <div className="line line-right-top"></div>
-        <div className="line line-right-bottom"></div>
-        <div className="line line-bottom"></div>
-        <div className="line line-right"></div>
-        <div className="line line-left-top"></div>
-        <div className="line line-center-bottom-left"></div>
-        <div className="line line-center-bottom-right"></div>
-        <div className="line line-center-top-right"></div>
-        <div className="line line-center-top-left"></div>
-        <div className="line line-center-top"></div>
-        <div className="line line-top"></div>
+        <span className="circle-big circle-top"></span>
+        <span className="circle-small circle-left-top"></span>
+        <span className="circle-small circle-right-top"></span>
+        <span className="circle-small circle-center"></span>
+        <span className="circle-big circle-left-bottom circle-gradient"></span>
+        <span className="circle-big circle-right-bottom"></span>
+        <span className="line line-right-top"></span>
+        <span className="line line-right-bottom"></span>
+        <span className="line line-bottom line-gradient-reverse"></span>
+        <span className="line line-right line-gradient"></span>
+        <span className="line line-left-top line-gradient"></span>
+        <span className="line line-center-bottom-left line-gradient"></span>
+        <span className="line line-center-bottom-right"></span>
+        <span className="line line-center-top-right"></span>
+        <span className="line line-center-top-left"></span>
+        <span className="line line-center-top"></span>
+        <span className="line line-top"></span>
       </div>
       <span className="headerLogo__text">graphiql</span>
     </Link>
