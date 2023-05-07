1cf95165f4f7a08a0176d8b220ca947afb6bbca7
AnatoliyIliev <vekok12@gmail.com>
2023-05-07 17:57:09 +0300

    feat: add SidePanel based on #9

---
+++ graphiql-app/src/components/SidePanel.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import '../styles/sidePanel.scss';
+
+function SidePanel() {
+  const [togglePanel, setTogglePanel] = useState(false);
+
+  const handleToggleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (event.currentTarget === event.target) {
+      return;
+    }
+    setTogglePanel(!togglePanel);
+  };
+
+  return (
+    <div className={`sidePanel ${togglePanel ? 'open' : ''}`} onClick={handleToggleClick}>
+      <div className="sidePanel-menu">
+        <div id="docs" className="sidePanel-menu__button">
+          Docs
+        </div>
+      </div>
+      <div>
+        <div className="sidePanel-info">Docs info</div>
+      </div>
+    </div>
+  );
+}
+
+export default SidePanel;
