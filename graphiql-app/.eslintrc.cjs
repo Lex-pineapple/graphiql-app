191c2b43995d7694cfac0259a3018c60d970363a
Lex <pineapplepear55@gmail.com>
2023-05-11 20:38:58 +0300

    feat: add schema fetch demo

--- graphiql-app/.eslintrc.cjs
+++ graphiql-app/.eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
+    // 'plugin:import/recommended',
     'plugin:import/typescript',
     'prettier',
     'plugin:react-hooks/recommended',
--- graphiql-app/src/components/SidePanel.tsx
+++ graphiql-app/src/components/SidePanel.tsx
@@ -1,8 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { request } from 'graphql-request';
 import '../styles/sidePanel.scss';
+import getIntrospectionQuery from '../graphql/getIntrospectionQuery';
 
 function SidePanel() {
   const [togglePanel, setTogglePanel] = useState(false);
+  const [query, setQuery] = useState('');
+
+  function getSchema() {
+    if (!query) {
+      request('https://swapi-graphql.netlify.app/.netlify/functions/index', getIntrospectionQuery())
+        .then((data) => {
+          console.log(data);
+          setQuery(JSON.stringify(data));
+        })
+        .catch((error) => console.error(error));
+    }
+  }
+
+  useEffect(() => {
+    getSchema();
+  })
 
   const handleToggleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (event.currentTarget === event.target) {
@@ -19,7 +37,7 @@ function SidePanel() {
         </div>
       </div>
       <div className="sidePanel-info">
-        <div>Docs info</div>
+        <div>{query}</div>
       </div>
     </div>
   );
