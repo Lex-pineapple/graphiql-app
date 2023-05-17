de6d4ad3ee32217d15e2d9545f4ae27a8d50755e
AnatoliyIliev <vekok12@gmail.com>
2023-05-17 11:20:00 +0300

    feat: add hero section in WelcomePage based on #16

--- graphiql-app/src/routes/WelcomePage.tsx
+++ graphiql-app/src/routes/WelcomePage.tsx
@@ -1,5 +1,86 @@
+import '../styles/welcomePage.scss';
+
 function WelcomePage() {
-  return <div>Welcome page</div>;
+  return (
+    <>
+      <section className="hero">
+        <div className="hero__container  container wrapper">
+          <div className="hero__info">
+            <div className="auth__img-bigtext">
+              <p className="auth__img-bigtext--highlight">GraphiQl</p>
+            </div>
+            <p className="auth__img-smalltext">
+              A powerful tool that offers syntax highlighting, intellisense autocompletion,
+              automatic documentation, and much more.
+            </p>
+            <button className="hero__button">Get Started</button>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h1>A query language for API</h1>
+          <p>
+            GraphQL is a query language for APIs and a runtime for fulfilling those queries. GraphQL
+            provides a complete and understandable description of the data in API, gives your the
+            power to ask for exactly what need and nothing more.
+          </p>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>Ask for what you need, get exactly that</h2>
+          <p>
+            Send a GraphQL query to our API and get exactly what you need, nothing more and nothing
+            less. GraphQL queries always return predictable results. Apps using GraphQL are fast and
+            stable because they control the data they get, not the server.
+          </p>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>Get many resources in a single request</h2>
+          <p>
+            GraphQL queries access not just the properties of one resource but also smoothly follow
+            references between them. While typical REST APIs require loading from multiple URLs,
+            GraphQL APIs get all the data our needs in a single request. App GraphQL quick even on
+            slow mobile network connections.
+          </p>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>About the course</h2>
+          <p>
+            <strong>RS School </strong>is free-of-charge and community-based education program
+            conducted by{' '}
+            <a target="_blank" href="https://rollingscopes.com/" rel="noreferrer">
+              The Rolling Scopes
+            </a>
+            developer community since 2013.
+          </p>
+          <p>
+            Everyone can study at RS School, regardless of age, professional employment, or place of
+            residence.
+          </p>
+          <p>
+            The mentors and trainers of our school are front-end and javascript developers from
+            different companies and countries.
+          </p>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>Our team</h2>
+          <ul>
+            <li>fdgsdfg</li>
+            <li>sgsdgdfg</li>
+            <li>sdfgsdfgdsf</li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default WelcomePage;
