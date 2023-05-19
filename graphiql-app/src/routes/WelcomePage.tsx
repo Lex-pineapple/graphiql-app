2b26efc6021145e812e125f1bf68cedf7fc06669
AnatoliyIliev <vekok12@gmail.com>
2023-05-19 13:28:16 +0300

    feat: add section about course in WelcomePage based on #16

--- graphiql-app/src/routes/WelcomePage.tsx
+++ graphiql-app/src/routes/WelcomePage.tsx
@@ -18,11 +18,12 @@ function WelcomePage() {
         </div>
       </section>
       <section className="aboutProject">
+        <h2 className="aboutCourse__title">About the QraphiQL</h2>
         <ul className="aboutProject__container container wrapper">
           <li className="aboutProject__item">
             <div className="aboutProject__img-query"></div>
             <div className="aboutProject__info">
-              <h2 className="aboutProject__title">A query language for API</h2>
+              <h3 className="aboutProject__title">A query language for API</h3>
               <p className="aboutProject__text">
                 GraphQL is a query language for APIs and a runtime for fulfilling those queries.
                 GraphQL provides a complete and understandable description of the data in API, gives
@@ -33,7 +34,7 @@ function WelcomePage() {
           <li className="aboutProject__item">
             <div className="aboutProject__img-what-you-need"></div>
             <div className="aboutProject__info">
-              <h2 className="aboutProject__title">Ask for what you need, get exactly that</h2>
+              <h3 className="aboutProject__title">Ask for what you need, get exactly that</h3>
               <p className="aboutProject__text">
                 Send a GraphQL query to our API and get exactly what you need, nothing more and
                 nothing less. GraphQL queries always return predictable results. Apps using GraphQL
@@ -44,7 +45,7 @@ function WelcomePage() {
           <li className="aboutProject__item">
             <div className="aboutProject__img-request"></div>
             <div className="aboutProject__info">
-              <h2 className="aboutProject__title">Get many resources in a single request</h2>
+              <h3 className="aboutProject__title">Get many resources in a single request</h3>
               <p className="aboutProject__text">
                 GraphQL queries access not just the properties of one resource but also smoothly
                 follow references between them. While typical REST APIs require loading from
@@ -55,34 +56,62 @@ function WelcomePage() {
           </li>
         </ul>
       </section>
-      <section>
-        <div>
-          <h2>About the course</h2>
-          <p>
-            <strong>RS School </strong>is free-of-charge and community-based education program
-            conducted by{' '}
-            <a target="_blank" href="https://rollingscopes.com/" rel="noreferrer">
-              The Rolling Scopes
-            </a>
-            developer community since 2013.
-          </p>
-          <p>
-            Everyone can study at RS School, regardless of age, professional employment, or place of
-            residence.
-          </p>
-          <p>
-            The mentors and trainers of our school are front-end and javascript developers from
-            different companies and countries.
-          </p>
+      <section className="aboutCourse">
+        <div className="aboutCourse__container container wrapper">
+          <h2 className="aboutCourse__title">About the course</h2>
+          <div className="aboutCourse__info">
+            <div className="aboutCourse__img"></div>
+            <div className="aboutCourse__block">
+              <p className="aboutCourse__text">
+                <strong className="aboutCourse__name">RS School </strong>is free-of-charge and
+                community-based education program conducted by{' '}
+                <a
+                  className="aboutCourse__RSlink"
+                  target="_blank"
+                  href="https://rollingscopes.com/"
+                  rel="noreferrer"
+                >
+                  The Rolling Scopes{' '}
+                </a>
+                developer community since 2013.
+              </p>
+              <p className="aboutCourse__text">
+                Everyone can study at RS School, regardless of age, professional employment, or
+                place of residence.
+              </p>
+              <p className="aboutCourse__text">
+                The mentors and trainers of our school are front-end and javascript developers from
+                different companies and countries.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
-      <section>
-        <div>
+      <section className="team">
+        <div className="team__container container wrapper">
           <h2>Our team</h2>
-          <ul>
-            <li>fdgsdfg</li>
-            <li>sgsdgdfg</li>
-            <li>sdfgsdfgdsf</li>
+          <ul className="team__list">
+            <li className="team__item">
+              <div className="team__img"></div>
+              <div>
+                <h3>name</h3>
+                <p>Frontend Developer</p>
+              </div>
+            </li>
+            <li className="team__item">
+              <div className="team__img"></div>
+              <div>
+                <h3>name</h3>
+                <p>Frontend Developer</p>
+              </div>
+            </li>
+            <li className="team__item">
+              <div className="team__img"></div>
+              <div>
+                <h3>name</h3>
+                <p>Frontend Developer</p>
+              </div>
+            </li>
           </ul>
         </div>
       </section>
