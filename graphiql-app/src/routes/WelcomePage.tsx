fd047a92329697919a7e57801a0db61a6a871bb0
AnatoliyIliev <vekok12@gmail.com>
2023-05-19 15:49:57 +0300

    feat: add section our team in WelcomePage based on #16

--- graphiql-app/src/routes/WelcomePage.tsx
+++ graphiql-app/src/routes/WelcomePage.tsx
@@ -18,7 +18,7 @@ function WelcomePage() {
         </div>
       </section>
       <section className="aboutProject">
-        <h2 className="aboutCourse__title">About the QraphiQL</h2>
+        <h2 className="aboutProject__main-title">About the QraphiQL</h2>
         <ul className="aboutProject__container container wrapper">
           <li className="aboutProject__item">
             <div className="aboutProject__img-query"></div>
@@ -60,7 +60,9 @@ function WelcomePage() {
         <div className="aboutCourse__container container wrapper">
           <h2 className="aboutCourse__title">About the course</h2>
           <div className="aboutCourse__info">
-            <div className="aboutCourse__img"></div>
+            <a target="_blank" href="https://rollingscopes.com/" rel="noreferrer">
+              <div className="aboutCourse__img"></div>
+            </a>
             <div className="aboutCourse__block">
               <p className="aboutCourse__text">
                 <strong className="aboutCourse__name">RS School </strong>is free-of-charge and
@@ -89,27 +91,114 @@ function WelcomePage() {
       </section>
       <section className="team">
         <div className="team__container container wrapper">
-          <h2>Our team</h2>
+          <h2 className="team__main-title">Our team</h2>
           <ul className="team__list">
             <li className="team__item">
-              <div className="team__img"></div>
-              <div>
-                <h3>name</h3>
-                <p>Frontend Developer</p>
+              <div className="team__img-Sasha"></div>
+              <div className="team__info">
+                <h3 className="team__name">Sasha Sonich</h3>
+                <p className="team__profession">Frontend Developer</p>
+                <div className="team__contacts">
+                  <a className="team__contacts-icon" href="mailto:pineapplepear55@gmail.com">
+                    <div className="team__chome"></div>
+                  </a>
+                  <a
+                    className="team__contacts-icon"
+                    target="_blank"
+                    href="https://t.me/TheShrimpy"
+                    rel="noreferrer"
+                  >
+                    <div className="team__telegram"></div>
+                  </a>
+                  <a
+                    className="team__contacts-icon"
+                    target="_blank"
+                    href="https://discord.com/invite/The Shrimpy#5845"
+                    rel="noreferrer"
+                  >
+                    <div className="team__discord"></div>
+                  </a>
+                  <a
+                    className="team__contacts-icon"
+                    target="_blank"
+                    href="https://github.com/Lex-pineapple"
+                    rel="noreferrer"
+                  >
+                    <div className="team__github"></div>
+                  </a>
+                </div>
               </div>
             </li>
             <li className="team__item">
-              <div className="team__img"></div>
-              <div>
-                <h3>name</h3>
-                <p>Frontend Developer</p>
+              <div className="team__img-Alixei"></div>
+              <div className="team__info">
+                <h3 className="team__name">Alexei Kozlovskiy</h3>
+                <p className="team__profession">Frontend Developer</p>
+                <div className="team__contacts">
+                  <a className="team__contacts-icon" href="mailto:geklgekleberefin@gmail.com">
+                    <div className="team__chome"></div>
+                  </a>
+                  <a
+                    className="team__contacts-icon"
+                    target="_blank"
+                    href="https://t.me/@Rubi_Rod"
+                    rel="noreferrer"
+                  >
+                    <div className="team__telegram"></div>
+                  </a>
+                  <a
+                    className="team__contacts-icon"
+                    target="_blank"
+                    href="https://discord.com/invite/FlexLex#6871"
+                    rel="noreferrer"
+                  >
+                    <div className="team__discord"></div>
+                  </a>
+                  <a
+                    className="team__contacts-icon"
+                    target="_blank"
+                    href="https://github.com/AlexeiKozlovskiy"
+                    rel="noreferrer"
+                  >
+                    <div className="team__github"></div>
+                  </a>
+                </div>
               </div>
             </li>
             <li className="team__item">
-              <div className="team__img"></div>
-              <div>
-                <h3>name</h3>
-                <p>Frontend Developer</p>
+              <div className="team__img-Anatoliy"></div>
+              <div className="team__info">
+                <h3 className="team__name">Anatoliy Iliev</h3>
+                <p className="team__profession">Frontend Developer</p>
+                <div className="team__contacts">
+                  <a className="team__contacts-icon" href="mailto:anatoliyiliiev@gmail.com">
+                    <div className="team__chome"></div>
+                  </a>
+                  <a
+                    className="team__contacts-icon"
+                    target="_blank"
+                    href="https://t.me/Anatoliy_Iliev"
+                    rel="noreferrer"
+                  >
+                    <div className="team__telegram"></div>
+                  </a>
+                  <a
+                    className="team__contacts-icon"
+                    target="_blank"
+                    href="https://discord.com/invite/AnatoliyIliev#0257"
+                    rel="noreferrer"
+                  >
+                    <div className="team__discord"></div>
+                  </a>
+                  <a
+                    className="team__contacts-icon"
+                    target="_blank"
+                    href="https://github.com/AnatoliyIliev"
+                    rel="noreferrer"
+                  >
+                    <div className="team__github"></div>
+                  </a>
+                </div>
               </div>
             </li>
           </ul>
